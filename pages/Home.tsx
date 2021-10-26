2ddebb0e299a4f14eb5acf0fcd85d5c4ab403b58
Justin Cano <jstncno@gmail.com>
2021-10-25 21:03:33 -0700

    Added more typography and content

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image';
 import styles from './Home.module.scss';
 import BackgroundGraphic from '../components/background-graphic/BackgroundGraphic';
 import Navbar from '../components/navbar/Navbar';
-import { H1, H2 } from '../components/typography';
+import { Hero, H1, H2 } from '../components/typography';
 
 const Home: NextPage = () => (
-  <div className={styles.container}>
+  <div className="flex flex-col justify-center mt-48">
     <Head>
       <title>JSTNCNO</title>
       <meta name="description" content="Justin Cano's Dev Blog" />
@@ -18,15 +18,24 @@ const Home: NextPage = () => (
     <BackgroundGraphic />
     <Navbar />
 
-    <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
-      <main className="md:col-span-5 lg:col-start-2 lg:col-span-2">
+    <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+      <section className="md:col-span-6 lg:col-start-2 lg:col-span-3">
+        <Hero>DEV BLOG</Hero>
+        <p className="font-sans text-primary dark:text-primary-dark">
+          Hi there, my name is Justin and I build things for the web.
+          <br/>
+          Welcome to my blog, where I ocassionally write about my experiences
+          as a software developer.
+        </p>
+      </section>
+      <main className="md:col-span-6 lg:col-start-2 lg:col-span-3">
         <H2>Recent</H2>
         {/* TODO: dynamically populate w/ components */}
         <section>
           <H1>Lorem ipsum dolor sit amet</H1>
         </section>
       </main>
-      <aside className="md:col-span-5 lg:col-start-4">
+      <aside className="md:col-span-6 lg:col-start-5 lg:col-span-2">
         <div className="grid grid-rows-2">
           <section className="row-start-1">
             <H2>Most Popular</H2>
