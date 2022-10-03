e4fb8faa5355810e449bff8596d818a5d2cca249
Justin Cano <jstncno@gmail.com>
2022-10-03 09:14:03 -0700

    Blog is main app for blog branch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from 'next/router';
 
 import '../styles/prism-tomorrow.min.css';
 import '../styles/globals.css';
@@ -6,14 +5,10 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import AppShell from '@jstncno/lib/layouts/AppShell';
-import SplashPage from '@jstncno/lib/components/splash-page/SplashPage';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const component = router.pathname === '/' ? <SplashPage /> :
-    <AppShell>
-      <Component {...pageProps} />
-    </AppShell>;
-  return component;
+  return <AppShell>
+    <Component {...pageProps} />
+  </AppShell>;
 }
 export default MyApp;
