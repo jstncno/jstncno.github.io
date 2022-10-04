d5cae2261cf4cd956abcfe015ad758a7f19d64fa
Justin Cano <jstncno@gmail.com>
2022-10-03 22:04:21 -0700

    Add more fonts

--- lib/components/splash-page/SplashPage.tsx
+++ lib/components/splash-page/SplashPage.tsx
@@ -38,6 +38,8 @@ export default function SplashPage() {
   return <>
     <Head>
       <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Arvo|Rokkitt|Slabo+27px|Poppins|Questrial|Raleway|" />
+      <link rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
         integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
         crossOrigin="anonymous" />
